f3ba6d2578710d8d48561222840ebbbec2f1bdc8
Dimasdit0 <136093175+Dimasdit0@users.noreply.github.com>
2024-12-06 21:51:54 +0700

    Delete PBO/Pertemuan 10 directory

--- PBO/Pertemuan 10/responsi/Buku.java
+++
@@ -1,7 +0,0 @@
-package responsi;
-
-public class Buku extends LibraryItem {
-    Buku(String judul, String pengarang, String kategori, String isbn){
-        super(judul, pengarang, kategori, isbn);
-    }
-}
